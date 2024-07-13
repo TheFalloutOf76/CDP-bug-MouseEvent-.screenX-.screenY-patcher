74b1e9015c9339e0105da1c62bf7d10944e01f28
TheFalloutOf76 <93239187+TheFalloutOf76@users.noreply.github.com>
2024-07-12 17:48:25 -0700

    Update manifest version to 3 and patch script.js

--- turnstilePatch/script.js
+++ turnstilePatch/script.js
@@ -1,15 +1,3 @@
-let patchScript = `
-Object.defineProperty(MouseEvent.prototype, 'screenX', {
-    value: 1234,
-    writable: false
-})
+Object.defineProperty(MouseEvent.prototype, 'screenX', { value: 1234 })
 
-Object.defineProperty(MouseEvent.prototype, 'screenY', {
-    value: 567,
-    writable: false
-})
-`;
-
-let element = document.createElement('script');
-element.innerHTML = patchScript;
-document.documentElement.appendChild(element);
+Object.defineProperty(MouseEvent.prototype, 'screenY', { value: 567 })
