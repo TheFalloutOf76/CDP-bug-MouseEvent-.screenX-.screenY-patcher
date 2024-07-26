32154b4fc9cdcb9a1c3b2e4566dbb0c31f8495fc
TheFalloutOf76 <93239187+TheFalloutOf76@users.noreply.github.com>
2024-07-26 16:40:50 -0700

    turns out 1234 567 is not very random...

--- turnstilePatch/script.js
+++ turnstilePatch/script.js
@@ -1,3 +1,10 @@
-Object.defineProperty(MouseEvent.prototype, 'screenX', { value: 1234 })
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-Object.defineProperty(MouseEvent.prototype, 'screenY', { value: 567 })
+let screenX = getRandomInt(800, 1200);
+let screenY = getRandomInt(400, 600);
+
+Object.defineProperty(MouseEvent.prototype, 'screenX', { value: screenX });
+
+Object.defineProperty(MouseEvent.prototype, 'screenY', { value: screenY });
